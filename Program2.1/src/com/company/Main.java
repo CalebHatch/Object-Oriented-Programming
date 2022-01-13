9ce63991f33396c473fe1e20fb8da654084e2661
CalebHatch <calebhatchcontact@gmail.com>
2022-01-13 04:17:25 -0600

    Polished some things

--- Program2.1/src/com/company/Main.java
+++ Program2.1/src/com/company/Main.java
@@ -5,9 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-	    System.out.println("This program converts a temperature in degrees Celsius into \n" +
-            "a temperature in degrees Fahrenheit. Enter a temperature in\n" +
-            "degrees Celsius: ");
+	    System.out.println("This program converts a temperature in degrees Celsius into " +
+                "a temperature in degrees Fahrenheit. Enter a temperature in degrees Celsius: ");
 
         double celsiusTemp = sc.nextDouble();
         double fahrenheitTemp = (celsiusTemp * 1.8) + 32;
